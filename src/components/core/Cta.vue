fc34f7196279fa7d4fd3067f40bc27884fbd95c4
yunli <diaolingzc@sina.com>
2020-06-27 08:56:42 +0800

    backTop

---
+++ src/components/core/Cta.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      v-show="isActive"
+      :style="styles"
+      color="#81D4FA"
+      fab
+      fixed
+      right
+      bottom
+      title="BackTop"
+      @click="$vuetify.goTo(0)"
+    >
+      <v-icon>
+        mdi-chevron-up
+      </v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+  export default {
+    name: 'CoreCta',
+
+    data () {
+      return {
+        isActive: false,
+      }
+    },
+
+    computed: {
+      styles () {
+        return {
+          marginTop: `${this.$vuetify.application.top}px`,
+        }
+      },
+    },
+
+    mounted () {
+      window.addEventListener('scroll', this.handleScroll)
+    },
+
+    methods: {
+      handleScroll () {
+        if (document.documentElement.scrollTop > 50) {
+          this.isActive = true
+        } else {
+          this.isActive = false
+        }
+      },
+    },
+  }
+</script>
